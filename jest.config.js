47435d2c3b264cb1a7b1ff7b15fff1ba40c735af
Goun Lee <goun.lee@lge.com>
2025-02-26 14:05:04 +0900

    Add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const config = {
+    displayName: "ilib-mono-webos root",
+	collectCoverage: true,
+    coverageReporters: [
+        "html",
+        "lcov",
+        "text",
+    ],
+}
+
+module.exports = config;
