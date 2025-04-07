0a0ec2a7572c15c97b86197de27dce9e53134a69
seonmi1.jin <seonmi1.jin@lge.com>
2025-04-07 14:08:15 +0900

    Update Copyright

--- packages/ilib-loctool-webos-json/JsonFile.js
+++ packages/ilib-loctool-webos-json/JsonFile.js
@@ -1,7 +1,7 @@
 /*
  * JsonFile.js - plugin to extract resources from a json code file
  *
- * Copyright (c) 2023 JEDLSoft
+ * Copyright (c) 2023,2025 JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
