2202c6c20ba4207056596cb6c132b4bc3d15335e
Goun Lee <goun.lee@lge.com>
2025-04-24 10:13:54 +0900

    test:jest.config.js coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 const config = {
     displayName: "ilib-mono-webos root",
 	collectCoverage: true,
+    coverageThreshold: {
+        global: {
+          branches: 30
+        }
+    },
     coverageReporters: [
         "html",
         "lcov",
