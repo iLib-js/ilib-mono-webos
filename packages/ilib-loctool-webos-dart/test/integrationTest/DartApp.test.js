e551aeba215b441b0470b8e55dcb33cdf15ed438
Goun Lee <goun.lee@lge.com>
2025-05-19 09:04:17 +0900

    Update DartApp.test.js

--- packages/ilib-loctool-webos-dart/test/integrationTest/DartApp.test.js
+++ packages/ilib-loctool-webos-dart/test/integrationTest/DartApp.test.js
@@ -27,7 +27,7 @@ describe('test the localization result of webos-dart app', () => {
     const projectRoot = (process.cwd().indexOf("integrationTest")) >-1 ? ".": "./test/integrationTest";
     const resourcePath = path.join(projectRoot, "assets/i18n");
     let filePath, jsonData = {};
-    console.log(process.cwd());
+
     beforeAll(async() => {
         /*if (fs.existsSync(resourcePath)) {
             fs.rmSync(resourcePath, { recursive: true });
@@ -99,9 +99,9 @@ describe('test the localization result of webos-dart app', () => {
         jsonData = pluginUtils.isValidPath(filePath) ? pluginUtils.loadData(filePath) : jsonData;
 
         expect(jsonData).toBeTruthy();
-        expect(jsonData["Search"]).toBe("통합 검색");
         expect(jsonData["App List"]).toBe("앱 목록");
         expect(jsonData["Back button"]).toBe("이전 버튼");
+        expect(jsonData["Search"]).toBe("통합 검색");
     });
     test("dartsample_test_es_CO", function() {
         expect.assertions(4);
@@ -109,8 +109,8 @@ describe('test the localization result of webos-dart app', () => {
         jsonData = pluginUtils.isValidPath(filePath) ? pluginUtils.loadData(filePath) : jsonData;
 
         expect(jsonData).toBeTruthy();
-        expect(jsonData["Search"]).toBe("Buscar");
         expect(jsonData["App List"]).toBe("Lista de Aplicaciones");
+        expect(jsonData["Search"]).toBe("Buscar");
         expect(jsonData["Back button"]).toBe("Botón regresar");
     });
     test("dartsample_test_es_US", function() {
@@ -137,80 +137,4 @@ describe('test the localization result of webos-dart app', () => {
         expect(jsonData).toBeTruthy();
         expect(jsonData["Programme"]).toBe("Programme");
     });
-    /*
-    test("dartsample_test_fr_CA", function() {
-        expect.assertions(6);
-        filePath = path.join(resourcePath, 'fr.json');
-        jsonData = isValidPath(filePath) ? loadData(filePath) : jsonData;
-
-        expect(jsonData).toBeTruthy();
-        expect(jsonData["App List"]).toBe("Liste des applications");
-        expect(jsonData["App Rating"]).toBe("Évaluation de l'application");
-        expect(jsonData["Back button"]).toBe("Bouton Retour");
-        expect(jsonData["Delete All"]).toBe("Tout supprimer");
-        expect(jsonData["Search_all"]).toBe("Rechercher");
-    });
-    test("dartsample_test_fr_FR", function() {
-        expect.assertions(6);
-        filePath = path.join(resourcePath, 'fr_FR.json');
-        jsonData = isValidPath(filePath) ? loadData(filePath) : jsonData;
-
-        expect(jsonData).toBeTruthy();
-        expect(jsonData["App List"]).toBe("Liste des applications");
-        expect(jsonData["App Rating"]).toBe("Évaluation de l'application");
-        expect(jsonData["Back button"]).toBe("Bouton Retour");
-        expect(jsonData["Delete All"]).toBe("Tout supprimer");
-        expect(jsonData["Search_all"]).toBe("Recherche");
-    });
-    
-    test("dartsample_test_es_ES", function() {
-        expect.assertions(7);
-        filePath = path.join(resourcePath, 'es_ES.json');
-        jsonData = isValidPath(filePath) ? loadData(filePath) : jsonData;
-
-        expect(jsonData).toBeTruthy();
-        expect(jsonData["App List"]).toBe("Lista de aplicaciones");
-        expect(jsonData["App Rating"]).toBe("Clasificación de la aplicación");
-        expect(jsonData["Back button"]).toBe("Botón atrás");
-        expect(jsonData["Delete All"]).toBe("Eliminar todo");
-        expect(jsonData["Search_all"]).toBe("Búsqueda");
-        expect(jsonData["OK"]).toBe("OK");
-    });
-    test("dartsample_test_en_US", function() {
-        expect.assertions(7);
-        filePath = path.join(resourcePath, 'en.json');
-        jsonData = isValidPath(filePath) ? loadData(filePath) : jsonData;
-
-        expect(jsonData).toBeTruthy();
-        expect(jsonData["App List"]).toBe("App List");
-        expect(jsonData["App Rating"]).toBe("App Rating");
-        expect(jsonData["Back button"]).toBe("Back button");
-        expect(jsonData["Delete All"]).toBe("Delete All");
-        expect(jsonData["Search_all"]).toBe("Search");
-        expect(jsonData["{appName} app cannot be deleted."]).toBe("{appName} app cannot be deleted.");
-    });
-    test("dartsample_test_ja_JP", function() {
-        expect.assertions(6);
-        filePath = path.join(resourcePath, 'ja.json');
-        jsonData = isValidPath(filePath) ? loadData(filePath) : jsonData;
-
-        expect(jsonData).toBeTruthy();
-        expect(jsonData["App List"]).toBe("アプリリスト");
-        expect(jsonData["App Rating"]).toBe("アプリの評価");
-        expect(jsonData["Back button"]).toBe("[戻る]ボタン");
-        expect(jsonData["Delete All"]).toBe("すべて削除");
-        expect(jsonData["Search_all"]).toBe("検索");
-    });
-    test("dartsample_test_sl_SI", function() {
-        expect.assertions(6);
-        filePath = path.join(resourcePath, 'sl.json');
-        jsonData = isValidPath(filePath) ? loadData(filePath) : jsonData;
-
-        expect(jsonData).toBeTruthy();
-        expect(jsonData["Search_all"]).toBe("Iskanje");
-        expect(jsonData["1#At least 1 letter|#At least {num} letters"].one).toBe("Vsaj {num} znak");
-        expect(jsonData["1#At least 1 letter|#At least {num} letters"].two).toBe("Vsaj {num} znaka");
-        expect(jsonData["1#At least 1 letter|#At least {num} letters"].few).toBe("Vsaj {num} znake");
-        expect(jsonData["1#At least 1 letter|#At least {num} letters"].other).toBe("Vsaj {num} znakov");
-    });*/
 });
